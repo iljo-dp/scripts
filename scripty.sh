63688ea2ff602b63102597d7d818668f912d6e80
iljo <59059625+iljo-dp@users.noreply.github.com>
2023-05-01 12:45:19 +0200

    Update scripty.sh

--- scripty.sh
+++ scripty.sh
@@ -3,16 +3,34 @@
 # Set the LAN-segment network address based on the project group
 # Replace the "172.18.0.0/16" with the appropriate network address for your group
 case "$1" in
-    1) LAN_SEGMENT="172.16.0.0/16";;
-    2) LAN_SEGMENT="172.18.0.0/16";;
-    3) LAN_SEGMENT="172.19.0.0/16";;
-    4) LAN_SEGMENT="172.20.0.0/16";;
-    5) LAN_SEGMENT="172.21.0.0/16";;
-    6) LAN_SEGMENT="172.22.0.0/16";;
-    7) LAN_SEGMENT="172.23.0.0/16";;
-    8) LAN_SEGMENT="172.24.0.0/16";;
-    *) echo "Usage: $0 [1-8]" >&2
-       exit 1;;
+    1)
+        LAN_SEGMENT="172.16.0.0/16"
+        ;;
+    2)
+        LAN_SEGMENT="172.18.0.0/16"
+        ;;
+    3)
+        LAN_SEGMENT="172.19.0.0/16"
+        ;;
+    4)
+        LAN_SEGMENT="172.20.0.0/16"
+        ;;
+    5)
+        LAN_SEGMENT="172.21.0.0/16"
+        ;;
+    6)
+        LAN_SEGMENT="172.22.0.0/16"
+        ;;
+    7)
+        LAN_SEGMENT="172.23.0.0/16"
+        ;;
+    8)
+        LAN_SEGMENT="172.24.0.0/16"
+        ;;
+    *)
+        echo "Usage: $0 [1-8]" >&2
+        exit 1
+        ;;
 esac
 
 # Get the current IP address and netmask for the LAN-segment interface
