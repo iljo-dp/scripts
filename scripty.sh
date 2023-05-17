2df7a40ed6fa5d561ec2facf186b9729881fa248
iljo <59059625+iljo-dp@users.noreply.github.com>
2023-05-17 11:43:54 +0200

    Update scripty.sh

--- scripty.sh
+++ scripty.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#Copyright (C) 2023 Iljo De Poorter
+    #This program is free software: you can redistribute it and/or modify
+    #it under the terms of the GNU General Public License as published by
+    #the Free Software Foundation, either version 3 of the License
+
+    #This program is distributed in the hope that it will be useful,
+    #but WITHOUT ANY WARRANTY; without even the implied warranty of
+    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    #GNU General Public License for more details.
+
+    #You should have received a copy of the GNU General Public License
+    #along with this program.If not, see<https://www.gnu.org/licenses/>.
+
+    #You can contact met at my electronic mail at Iljodp@gmail.com
+
+
+#START OF THE PROGRAM
 apt-get install fish exa curl isc-dhcp-server bind9 bind9-doc dnsutils
 cp config.fish /root/.config/fish
 source /root/.config/fish/config.fish
