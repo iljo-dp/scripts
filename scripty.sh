0438a1baa93a2e27c1febff10bf6249fb9dbe663
iljo <59059625+iljo-dp@users.noreply.github.com>
2023-05-17 11:51:32 +0200

    complete rewrite of the codebase(untested)

--- scripty.sh
+++ scripty.sh
@@ -1,88 +1,69 @@
 #!/bin/bash
 
-#Copyright (C) 2023 Iljo De Poorter
-    #This program is free software: you can redistribute it and/or modify
-    #it under the terms of the GNU General Public License as published by
-    #the Free Software Foundation, either version 3 of the License
+# Copyright (C) 2023 Iljo De Poorter
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# You can contact me at my electronic mail at Iljodp@gmail.com
 
-    #This program is distributed in the hope that it will be useful,
-    #but WITHOUT ANY WARRANTY; without even the implied warranty of
-    #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-    #GNU General Public License for more details.
+# Install required packages
+apt-get install -y fish exa curl isc-dhcp-server bind9 bind9-doc dnsutils
 
-    #You should have received a copy of the GNU General Public License
-    #along with this program.If not, see<https://www.gnu.org/licenses/>.
-
-    #You can contact met at my electronic mail at Iljodp@gmail.com
-
-
-#START OF THE PROGRAM
-apt-get install fish exa curl isc-dhcp-server bind9 bind9-doc dnsutils
+# Copy Fish shell configuration
 cp config.fish /root/.config/fish
 source /root/.config/fish/config.fish
 
+# Change the default shell for root user to Fish
 chsh root -s /usr/bin/fish
 
+# Install Starship prompt
 curl -Ss https://starship.rs/install.sh | sh
 
-
 # Create groups
-groupadd zaakvoerder
-groupadd klantenrelaties
-groupadd administratie
-groupadd IT_medewerker
+for group in zaakvoerder klantenrelaties administratie IT_medewerker; do
+    groupadd "$group"
+done
 
-# Add users
-# Prompt for the first user's name
-read -p "Please enter the full name for user 1: " name1
+# Function to create user
+create_user() {
+    local username=$1
+    local full_name=$2
+    local group=$3
+    local password="${username}123"
+    local last_name_initial
 
-# Split the name into first and last name parts
-IFS=' ' read -ra name_parts <<< "$name1"
-first_name="${name_parts[0]}"
+    IFS=' ' read -ra name_parts <<< "$full_name"
+    first_name="${name_parts[0]}"
 
-# Generate the password for user 1
-password="${first_name}123"
-if [[ ${#name_parts[@]} -gt 1 ]]; then
-    last_name_initial=$(echo "${name_parts[@]:1}" | awk -F' ' '{for (i=1;i<=NF;i++) print substr($i,1,1)}' | tr -d '\n' | tr '[:upper:]' '[:lower:]')
-    password="${password}${last_name_initial}"
-fi
+    if [[ ${#name_parts[@]} -gt 1 ]]; then
+        last_name_initial=$(echo "${name_parts[@]:1}" | awk -F' ' '{for (i=1;i<=NF;i++) print substr($i,1,1)}' | tr -d '\n' | tr '[:upper:]' '[:lower:]')
+        password="${password}${last_name_initial}"
+    fi
 
-# Create user 1 using the generated password
-useradd -m -c "$name1" -s /bin/bash -g zaakvoerder -p $(openssl passwd -1 "$password") "${first_name,,}${last_name_initial,,}"
+    useradd -m -c "$full_name" -s /bin/bash -g "$group" -p $(openssl passwd -1 "$password") "${username,,}${last_name_initial,,}"
+    chown -R "${username,,}${last_name_initial,,}":"$group" "/home/${username,,}${last_name_initial,,}"
+}
 
-# Prompt for the second user's name
-read -p "Please enter the full name for user 2: " name2
+# Prompt for user creation
+for i in 1 2; do
+    read -p "Please enter the full name for user $i: " name
+    create_user "user$i" "$name" "zaakvoerder"
+done
 
-# Split the name into first and last name parts for user 2
-IFS=' ' read -ra name_parts <<< "$name2"
-first_name="${name_parts[0]}"
+# Create additional users
+create_user "tinevdv" "Tine Van de Velde" "klantenrelaties"
+create_user "jorisq" "Joris Quataert" "administratie"
+create_user "kimdw" "Kim De Waele" "IT_medewerker"
 
-# Generate the password for user 2
-password="${first_name}123"
-if [[ ${#name_parts[@]} -gt 1 ]]; then
-    last_name_initial=$(echo "${name_parts[@]:1}" | awk -F' ' '{for (i=1;i<=NF;i++) print substr($i,1,1)}' | tr -d '\n' | tr '[:upper:]' '[:lower:]')
-    password="${password}${last_name_initial}"
-fi
-
-# Create user 2 using the generated password
-useradd -m -c "$name2" -s /bin/bash -g zaakvoerder -p $(openssl passwd -1 "$password") "${first_name,,}${last_name_initial,,}"
-
-# Change ownership of the home directories to the users
-chown -R "${first_name,,}${last_name_initial,,}":zaakvoerder /home/"${first_name,,}${last_name_initial,,}"
-
-
-useradd -m -c "Tine Van de Velde" -s /bin/bash -g klantenrelaties -p $(openssl passwd -1 tine123) tinevdv
-useradd -m -c "Joris Quataert" -s /bin/bash -g administratie -p $(openssl passwd -1 joris123) jorisq
-useradd -m -c "Kim De Waele" -s /bin/bash -g IT_medewerker -p $(openssl passwd -1 kim123) kimdw
-
-# Change ownership of the home directories to the users
-chown -R tinevdv:klantenrelaties /home/tinevdv
-chown -R jorisq:administratie /home/jorisq
-chown -R kimdw:IT_medewerker /home/kimdw
-
-
-file_path="/etc/network/interfaces"
-new_content=$(cat <<EOF
+# Configure network interfaces
+interfaces_file="/etc/network/interfaces"
+interfaces_content=$(cat <<EOF
 source /etc/network/interfaces.d/*
 auto lo
 iface lo inet loopback
@@ -102,22 +83,21 @@ EOF
 )
 
 # Backup the original file (optional but recommended)
-cp "$file_path" "$file_path.bak"
+cp "$interfaces_file" "$interfaces_file.bak"
 
 # Write the new content to the file
-echo "$new_content" > "$file_path "
+echo "$interfaces_content" > "$interfaces_file"
 
-#netwerkkaart instellen
-the_path="/etc/default/isc-dhcp-server"
-new_content2=$(cat <<EOF
+# Configure ISC DHCP Server
+dhcp_server_file="/etc/default/isc-dhcp-server"
+dhcp_server_content=$(cat <<EOF
 
 INTERFACESv4="ens36"
 EOF
 )
 
 # Backup the original file (optional but recommended)
-cp "$the_path" "$the_path.bak"
+cp "$dhcp_server_file" "$dhcp_server_file.bak"
 
 # Write the new content to the file
-
-echo "$new_content2" > "$the_path"
+echo "$dhcp_server_content" > "$dhcp_server_file"
